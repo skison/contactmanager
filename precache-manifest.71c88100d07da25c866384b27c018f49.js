05939bf5c52479354a4985ca26fd532844980497
skison <samuel.j.kison@wmich.edu>
2019-05-10 13:50:31 -0400

    Updates

---
+++ precache-manifest.71c88100d07da25c866384b27c018f49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad9c6456225e1d074b1a0543223ea90e",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "b4aecc3cb930cd7f0bc8",
+    "url": "/contactmanager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "860fbaee0aa4a2f164e5",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "b4aecc3cb930cd7f0bc8",
+    "url": "/contactmanager/static/js/2.d7f3f934.chunk.js"
+  },
+  {
+    "revision": "860fbaee0aa4a2f164e5",
+    "url": "/contactmanager/static/js/main.81fbe25a.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  }
+]);
